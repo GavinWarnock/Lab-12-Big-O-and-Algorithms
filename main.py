7c38f764a6bbc89e3c2086fa775111b64bc47d17
Gavin Warnock <gbwarnock@gmail.com>
2023-02-23 12:46:18 -0700

    Completed 2nd task and began working on task 3

--- main.py
+++ main.py
@@ -7,13 +7,24 @@ def even_or_odd(number):
         return False
 print(even_or_odd(13))
 
-
+# O(n) Time Complexity
 
 def less_than_100(list_of_numbers):
     for number in list_of_numbers:
-        if len(number) > 100:
-            return False
-        elif number <= 100:
-            return True
+        if number > 99:
+            return False    
+    return True
 print(less_than_100([1, 2, 3, 4, 14]))
 
+
+
+def repeated_names(list_of_names):
+    for name in list_of_names:
+        is_duplicate = False
+        while is_duplicate == False:
+            if name == name:
+                is_duplicate = True
+    return is_duplicate
+
+
+print(repeated_names(["Gavin", "Brittany", "Gwen", "Sonja", "Modi", "Meena", "Modi"]))
