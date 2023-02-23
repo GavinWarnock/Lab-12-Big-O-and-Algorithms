539ac64f19fd3a8146112f8931ea1dc752cb0bb6
Gavin Warnock <gbwarnock@gmail.com>
2023-02-23 10:50:17 -0700

    Refactored first task function and started on second task function

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-def even_or_odd():
-    number = int(input("What number would you like to check "))
+# O(1) Time Complexity
+
+def even_or_odd(number):
     if (number % 2) == 0:
         return True
     else:
         return False
-print(even_or_odd())
+print(even_or_odd(13))
+
+
+
+def less_than_100(list_of_numbers):
+    for number in list_of_numbers:
+        if number > 100:
+            return False
+        else:
+            return True
+print(less_than_100([1, 2, 3, 4, 500]))
+
